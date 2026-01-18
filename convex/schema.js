e561af7f846039af11c515d3bc2e10a23983ec3c
shashwat shukla <shuklashashwat009@gmail.com>
2026-01-19 01:49:35 +0530

    schema update

--- convex/schema.js
+++ convex/schema.js
@@ -27,4 +27,74 @@ export default defineSchema({
         createdAt:v.number(),
         updatedAt:v.number(),
     }).index("by_token",["tokenIdentifier"]),
-})
+
+    events:defineTable({
+      title:v.string(),
+      description:v.string(),
+      slug:v.string(),
+
+      //org
+      organizerId:v.id("users"),
+      organizerName:v.string(),
+
+      //eve details
+      category:v.string(),
+      tags:v.array(v.string()),
+
+      //date time
+      startDate:v.number(),
+      endDate:v.number(),
+      timezone:v.string(),
+
+      //location
+      locationType:v.union(v.literal("physical"),v.literal("online")),
+      venue:v.optional(v.string()),
+      address:v.optional(v.string()),
+      city:v.string(),
+      state:v.optional(v.string()),
+
+      //cap
+      capacity:v.number(),
+      ticketType:v.union(v.literal("free"),v.literal("paid")),
+      ticketPrice:v.optional(v.number()),//paid or free
+      registrationCount:v.number(),
+
+      //custom
+      coverImage:v.optional(v.string()),
+      themeColor:v.optional(v.string()),
+      
+      //time
+      createdAt:v.number(),
+      updatedAt:v.number()
+    })
+    .index("by_organizer", ["organizerId"])
+    .index("by_category", ["category"])
+    .index("by_start_date", ["startDate"])
+    .index("by_slug", ["slug"])
+    .searchIndex("search_title", { searchField: "title" }),
+
+    registrations: defineTable({
+    eventId: v.id("events"),
+    userId: v.id("users"),
+
+    // Attendee info
+    attendeeName: v.string(),
+    attendeeEmail: v.string(),
+
+    // QR Code for entry
+    qrCode: v.string(), // Unique ID for QR
+
+    // Check-in
+    checkedIn: v.boolean(),
+    checkedInAt: v.optional(v.number()),
+
+    // Status
+    status: v.union(v.literal("confirmed"), v.literal("cancelled")),
+
+    registeredAt: v.number(),
+  })
+    .index("by_event", ["eventId"])
+    .index("by_user", ["userId"])
+    .index("by_event_user", ["eventId", "userId"])
+    .index("by_qr_code", ["qrCode"]),
+});
