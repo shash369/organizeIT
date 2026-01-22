267da2ccb8569c688bc9520a98883ac3d3ab96a3
shashwat shukla <shuklashashwat009@gmail.com>
2026-01-22 23:06:59 +0530

    proxy update

--- proxy.js
+++ proxy.js
@@ -1,27 +1,28 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute=createRouteMatcher([
-   "/my-events(.*)",
-   "/create-events(.*)",
-   "/my-tickets(.*)",
+const isProtectedRoute = createRouteMatcher([
+  "/my-events(.*)",
+  "/create-event(.*)",
+  "/my-tickets(.*)",
 ]);
 
-export default clerkMiddleware(async(auth,req)=>{
-   const {userId,redirectToSignIn}=await auth();
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
 
-   if(!userId && isProtectedRoute(req)){
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
     return redirectToSignIn();
-   }
+  }
 
-   return NextResponse.next();
+  return NextResponse.next();
 });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
