9a8984aeb7f8ddd60e9400713950ddef390222c0
shashwat shukla <shuklashashwat009@gmail.com>
2026-01-22 23:09:04 +0530

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    domains: ["images.unsplash.com", "img.clerk.com"],
+  },
 };
 
 export default nextConfig;
